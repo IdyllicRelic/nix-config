b18dbccb5a3501b391346310f4675b76256402ef
Seyrn <147831357+IdyllicRelic@users.noreply.github.com>
2026-05-14 15:17:02 +0530

    update

--- system/network.nix
+++ system/network.nix
@@ -1,11 +1,17 @@
 { ... }:
 {
   networking = {
-    networkmanager.enable = true;
-    nameservers = [               # Sets dns to cloudflare block malware
-      "1.1.1.2" "1.0.0.2"
-      "2606:4700:4700::1112" "2606:4700:4700::1002"
+    networkmanager = {
+      enable = true;
+      dns = "none";
+      wifi.powersave = false;
+    };
+    nameservers = [
+      # Sets dns to cloudflare block malware
+      "1.1.1.2"
+      "1.0.0.2"
+      "2606:4700:4700::1112"
+      "2606:4700:4700::1002"
     ];
-    networkmanager.dns = "none";
   };
 }
